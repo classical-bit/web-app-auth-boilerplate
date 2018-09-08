190faf1e30aa4236326977d5cc33dcd32e46ec22
developersTogether <deepanshu.mishra0@gmail.com>
2018-09-08 13:14:26 +0530

    A Fresh Start

--- api/routes/UserRoute.js
+++
@@ -1,9 +0,0 @@
-const UserController = require('../controllers/UserController');
-const router = require('express-promise-router')();
-
-router.route('/signup')
-    .post(UserController.signUp);
-router.route('/signin')
-    .post(UserController.signIn);
-
-module.exports = router;
--- index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app = require('./api/app');
-
-const port = process.env.API_PORT || 5000;
-
-app.listen(port);
-console.log(`API @ ${port}`);
